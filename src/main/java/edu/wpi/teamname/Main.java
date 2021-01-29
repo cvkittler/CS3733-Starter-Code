86cf148d3053b03473fac509e3f3dfeb8845bbdc
cvkittler <cvkittler@wpi.edu>
2021-01-29 14:54:04 -0500

    added name

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -1,7 +1,9 @@
 package edu.wpi.teamname;
 
 public class Main {
-
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
   public static void main(String[] args) {
     App.launch(App.class, args);
   }
